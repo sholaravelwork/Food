14f690d72f6032ebb8244f8df365f8659a6a533e
織田 <odasho1133@gmail.com>
2022-11-06 11:40:10 +0900

    おすすめコースのメニューid修正

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -24,7 +24,7 @@ class PostController extends Controller
 // ホーム画面に遷移
    public function index() {
 
-    $menus =Menu::find([22,23,24]);
+    $menus =Menu::find([2,3,4]);
 
     return view('index')
     ->with(['menus' => $menus]);
